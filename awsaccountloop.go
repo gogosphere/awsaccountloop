bfbfd980c66faa414c7e8879c80882d9d6d0bc5f
GoGoSphere <gogosphere@gmail.com>
2016-11-10 18:43:06 -0600

    Rewrite for better usability

--- awsaccountloop.go
+++ awsaccountloop.go
@@ -1,78 +1,78 @@
 package awsaccountloop
 
 import (
-	"bufio"
-	"log"
 	"os"
-	"regexp"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/iam"
 )
 
-var (
-	credentialsPath = os.Getenv("HOME") + "/.aws/credentials"
-	Accountcreds    = make(map[*ec2.EC2]string)
-	r               = readFile(credentialsPath)
-	p               = pullAccounts(r)
-)
+var accounts = []string{""}
+var regions = []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-south-1", "ap-northeast-2",
+	"ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ec2.eu-central-1.amazonaws.com", "ec2.eu-west-1.amazonaws.com", "sa-east-1"}
+var credentialsfile = os.Getenv("HOME") + "/.aws/credentials"
 
-// AWSAccount holding the account number and the svc token here
-type AWSAccount struct {
-	Accountcreds    map[string]*ec2.EC2
-	accountnames    []string
-	credentialsPath string
+type RawCreds struct {
+	Creds    map[string]*ec2.EC2
+	Sessions map[string]*session.Session
+	Iams     map[string]*iam.IAM
 }
 
-func New() *AWSAccount {
-	var awsloop = &AWSAccount{
-		credentialsPath: credentialsPath,
-		accountnames:    p,
-		Accountcreds:    assignToken(),
-	}
-	return awsloop
+type RCInterface interface {
+	CAssign()
+	IAssign()
+	SAssign()
 }
 
-func readFile(cf string) []string {
-	var f []string
-	file, err := os.Open(cf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+/*
+ */
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		f = append(f, scanner.Text())
-	}
-	return f
+func New() *RawCreds {
+	rc := &RawCreds{}
+	rc.CAssign()
+	rc.IAssign()
+	rc.SAssign()
+	return rc
 }
-func pullAccounts(pa []string) []string {
-	r := regexp.MustCompile("[a-z]+-[a-z]+_.*admin")
-	var a []string
-	for _, v := range pa {
-		match := r.FindString(v)
-		if len(match) != 0 {
-			a = append(a, match)
-		}
-	}
-	return a
-}
-func assignToken() map[string]*ec2.EC2 {
+
+func (r *RawCreds) CAssign() {
 	Accountcreds := make(map[string]*ec2.EC2)
-	for _, v := range p {
-		credentialObject := credentials.NewSharedCredentials(credentialsPath, v)
-		svc := ec2.New(session.New(aws.NewConfig().WithRegion("us-east-1").
-			WithMaxRetries(2).WithCredentials(credentialObject)))
+
+	for _, v := range accounts {
+
+		credentialObject := credentials.NewSharedCredentials(credentialsfile, v)
+		svc := ec2.New(session.New(aws.NewConfig().WithRegion("us-east-1").WithMaxRetries(2).WithCredentials(credentialObject)))
 		Accountcreds[v] = svc
 	}
-	return Accountcreds
+
+	r.Creds = Accountcreds
 }
 
-func er(e error) {
-	if e != nil {
-		log.Printf("Error: %v\n", e)
+func (r *RawCreds) IAssign() {
+	Accountcreds := make(map[string]*iam.IAM)
+
+	for _, v := range accounts {
+
+		credentialObject := credentials.NewSharedCredentials(credentialsfile, v)
+		svc := iam.New(session.New(aws.NewConfig().WithRegion("us-east-1").WithMaxRetries(2).WithCredentials(credentialObject)))
+		Accountcreds[v] = svc
 	}
+
+	r.Iams = Accountcreds
+}
+
+func (r *RawCreds) SAssign() {
+	Sessioncreds := make(map[string]*session.Session)
+
+	for _, v := range accounts {
+
+		credentialObject := credentials.NewSharedCredentials(credentialsfile, v)
+		sess := session.New(aws.NewConfig().WithRegion("us-east-1").WithMaxRetries(2).WithCredentials(credentialObject))
+		Sessioncreds[v] = sess
+	}
+
+	r.Sessions = Sessioncreds
 }
